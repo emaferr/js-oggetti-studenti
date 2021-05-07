023e2a8d105b39e655586f2aa4713df5b888d441
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-07 16:34:05 +0200

    Stampo a schermno con un ciclo for in tutte le propietà di Studenti

--- main.js
+++ main.js
@@ -4,14 +4,27 @@ var studenti = {
 
     nome : "Tony" ,
     cognome : "Stark" ,
-    età : 54
+    età : 54 + " anni"
 
 }
 
 // Stampo a schermno con un ciclo for in tutte le propietà di Studenti
 
+var studenteSingolo = document.getElementById( "studente" ) ; 
+
 for ( var dato in studenti) {
 
     console.log( dato , studenti [ dato ]);
+
+    studenteSingolo.insertAdjacentHTML( 'beforeend' , `
+
+    <li class="list-group-item d-flex w-100 justify-content-between" >
+
+        <span class="text-uppercase text-monospace font-weight-bold" > ${ dato } </span> 
+        <span class="text-monospace" > ${ studenti[ dato ] }</span>  
+
+    </li>
     
+    `)
+
 }
