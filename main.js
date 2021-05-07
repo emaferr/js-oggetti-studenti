646139c3a1dfa92a4c9d97a975ed3d1090c2e133
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-05-07 17:47:08 +0200

    Creo array oggetti ciclo e stampo due chiavi di ogni oggetto

--- main.js
+++ main.js
@@ -14,10 +14,9 @@ var studenteSingolo = document.getElementById( "studente" ) ;
 
 for ( var dato in studenti) {
 
-    console.log( dato , studenti [ dato ]);
-
-    studenteSingolo.insertAdjacentHTML( 'beforeend' , `
-
+    studenteSingolo.insertAdjacentHTML( 'beforeend' , 
+    
+    `
     <li class="list-group-item d-flex w-100 justify-content-between" >
 
         <span class="text-uppercase text-monospace font-weight-bold" > ${ dato } </span> 
@@ -27,4 +26,64 @@ for ( var dato in studenti) {
     
     `)
 
+}
+
+// Definisco un array di oggetti (Studenti)
+
+var classe = [ 
+    
+    {
+        nomeStudente : "Steve" ,
+        cognomeStudente : "Rogers" ,
+        età : 38 + " anni"
+
+    } ,
+
+    {
+        nomeStudente : "Bruce" ,
+        cognomeStudente : "Banner" ,
+        età : 41 + " anni"
+
+    } ,
+
+    {
+        nomeStudente : "Peter" ,
+        cognomeStudente : "Parker" ,
+        età : 22 + " anni"
+
+    } ,
+
+    {
+        nomeStudente : "Natasha" ,
+        cognomeStudente : "Roomanoff" ,
+        età : 31 + " anni"
+
+    } 
+
+] ;
+
+// Ciclo e stampo nome e cognome di ogni studente
+
+var sectionEl = document.getElementById( "studente_classe" ) ;
+
+var i = 0;
+
+while ( i < classe.length) {
+
+    var studenteClasse = classe[i] ;
+
+        sectionEl.insertAdjacentHTML( 'beforeend' , 
+
+        `
+        <li class="list-group-item d-flex w-100 justify-content-between">
+
+            <span class="text-uppercase text-monospace font-weight-bold"> Studente </span>
+            <span class="text-monospace"> ${ studenteClasse.nomeStudente } </span>
+            <span class="text-monospace"> ${ studenteClasse.cognomeStudente } </span>
+    
+        </li>
+
+        `)
+
+    i++
 }
